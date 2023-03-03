a4e62c29dc0370350712c8780e3499f03796b9b0
Joshua Angeles <mintoesverm@yahoo.com>
2023-03-03 01:09:53 +0000

    Added list and isList variables.

--- server/commands/import.js
+++ server/commands/import.js
@@ -5,9 +5,15 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('import')
     .setDescription('Imports emojis from EL Library. (As in EmojiLib, not La Biblioteca)')
-    .addStringOption(option => option.setName('name').setDescription('The emojis to be imported').setRequired(true)),
+    .addStringOption(option => option.setName('list').setDescription('The emoji(s) to be imported').setRequired(true)),
   async execute(interaction) {
     // use discord.js's createGuildEmoji and relevant Managers to create each emoji.
-    // Probably needs to use a loop if Discord API caps post requests...
+    // Take the message content and split the `name(s)` string (String.split()) and store returned emoji object array.
+    // Query getByNames endpoint for the returned array
+    // Call the create method of discord.js's GuildEmojiManager for each emoji objects' url property.
+    const list = interaction.getString('list');
+    // eslint-disable-next-line no-unused-vars
+    const isList = list.includes(',');
+
   }
 };
