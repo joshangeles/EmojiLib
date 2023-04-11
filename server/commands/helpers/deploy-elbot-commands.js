b62c0b9612a8d1ccbcc112eb2d0870eba64964e2
Joshua Angeles <mintoesverm@yahoo.com>
2023-04-11 03:42:52 +0000

    Polished and removed config.json

--- server/commands/helpers/deploy-elbot-commands.js
+++ server/commands/helpers/deploy-elbot-commands.js
@@ -1,13 +1,12 @@
 /* eslint-disable no-console */
 const { REST, Routes } = require('discord.js');
-const { clientId, token } = require('../../config.json');
 const fs = require('node:fs');
 
 const commands = [];
 // Grabs all command files from the commands directory
 const commandsPath = 'server/commands';
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN);
 
 for (const file of commandFiles) {
   const command = require(`../${file}`);
@@ -21,7 +20,7 @@ for (const file of commandFiles) {
     // The put method is used to fully refresh all commands in the server with
     // the current set within the commands array.
     const data = await rest.put(
-      Routes.applicationCommands(clientId),
+      Routes.applicationCommands(process.env.CLIENT_ID),
       { body: commands }
     );
       // NOTE: Registered commands are stored by Discord, so this only needs to
--- server/commands/helpers/remove-elbot-commands.js
+++ server/commands/helpers/remove-elbot-commands.js
@@ -1,8 +1,7 @@
 /* eslint-disable no-console */
 // For deleting registered slash commands. NOTE: For development purposes only!
 const { REST, Routes } = require('discord.js');
-const { clientId, token } = require('../../config.json');
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN);
 
 /**
  * An array containing the commandIDs for all commands flagged for deletion.
@@ -15,14 +14,14 @@ for (const commandId of commands) {
   // This timeout is set to buffer delete requests to prevent rejections.
   setTimeout(() => {
     // This code block send a delete request to the Discord API for each commandId found in the commands array.
-    rest.delete(Routes.applicationCommand(clientId, `${commandId}`))
+    rest.delete(Routes.applicationCommand(process.env.CLIENT_ID, `${commandId}`))
       .then(() => console.log(`Successfully deleted application command #${commandId}`))
       .catch(console.error);
   }, 1000);
 }
 
 if (commands.length === 0) {
-  rest.put(Routes.applicationCommands(clientId), { body: [] })
+  rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: [] })
     .then(() => console.log('Successfully deleted all application commands.'))
     .catch(console.error);
 }
