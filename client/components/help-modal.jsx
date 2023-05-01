558e7843eb649973d15205e013a82587cbdb88e1
Joshua Angeles <mintoesverm@yahoo.com>
2023-05-01 06:02:07 +0000

    Changed the help modal to use a numbered list for easier reading.

--- client/components/help-modal.jsx
+++ client/components/help-modal.jsx
@@ -1,33 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 export default function HelpModal() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div
-      className="modal show"
-      style={{ display: 'block', position: 'initial' }}
-    >
-      <Modal.Dialog>
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Need Help?</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <h3>Installation:</h3>
-          <p>Trying to add the bot to your server? Click <a href='https://discord.com/api/oauth2/authorize?client_id=1074898581758148729&permissions=8&scope=bot'>here!</a></p>
+          <p>Trying to add the bot to your server? Click <a href='https://discord.com/api/oauth2/authorize?client_id=1080643273749700719&permissions=8&scope=bot'>here!</a></p>
           <br />
           <h3>How to use:</h3>
-          <p>
-            After adding the bot to your server, go to the <a href='https://emoji-lib.herokuapp.com/'>Library</a>
-            and select your emojis! (They will highlight white when selected)
-            After, click the import button and the command for the emojis will be added to your clipboard.
-            Finally, simply copy-and-paste the command in a channel the bot can see, and send! The bot will tell you when your emojis are imported!
-          </p>
+          <ol>
+            <li>Add the bot to your server</li>
+            <li>Go to the <a href='https://emoji-lib.herokuapp.com/'>Library</a></li>
+            <li>Select your desired emotes</li>
+            <li>Click the import button</li>
+            <li>Paste the command in your server</li>
+          </ol>
+          <p>The EmojiLib bot will let you know when your emojis are ready!</p>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary">Close</Button>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
         </Modal.Footer>
-      </Modal.Dialog>
-    </div>
+      </Modal>
+    </>
   );
 }
