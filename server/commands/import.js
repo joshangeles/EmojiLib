263c89bf48d9fa52d8b8889e407f94f5ef38afc7
Joshua Angeles <mintoesverm@yahoo.com>
2023-05-05 06:23:51 +0000

    Changed file path to url

--- server/commands/import.js
+++ server/commands/import.js
@@ -46,8 +46,7 @@ module.exports = {
            * @property emoteObject.attachment - The filepath to the emoji being imported.
            * @property emoteObject.name - The name of the emoji being imported.
            */
-          console.log(window.location.pathname);
-          const emoteObject = { attachment: `${data.url}`, name: `${data.name}` };
+          const emoteObject = { attachment: `https://emoji-lib.herokuapp.com${data.url}`, name: `${data.name}` };
           console.log('Attachment:', emoteObject.attachment);
           interaction.guild.emojis.create(emoteObject)
             .then(emote => {
